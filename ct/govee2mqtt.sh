221dc53bd93db133d2f2b3db50e57a07dd666d3b
sitapix <sitayo.exe@gmail.com>
2025-10-01 22:48:50 -0500

    Update govee2mqtt.sh to use sitapix fork URLs for testing

--- ct/govee2mqtt.sh
+++ ct/govee2mqtt.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/sitapix/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/sitapix/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/wez/govee2mqtt
 
 APP="Govee2MQTT"
